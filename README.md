67357a910cc1d758647a7877cd575b48ed848ee6
Patricia Nelson <urbanearthymama@gmail.com>
2021-08-29 23:33:14 -0500

    restructure with Rommel

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Challenge 3 - Password Generator
 
-Asked to create a random password generator that required an at least 8 characters no more than 128. Users are to be prompted with multiple options to refine their password strength through varied degrees of complexities. Once presented with the array of options the generator would produce and unique random password.
+For this challenge the developer was asked to create a random password generator that required an at least 8 characters no more than 128. Users are to be prompted with multiple options to refine their password strength through varied degrees of complexities. Once presented with the array of options the generator would produce and unique random password.
 
-Link of deployed URL
+###Link:
+https://urbanearthymama.github.io/challenge-3/
 
-Screenshot
+![Screen shot](./assets/images/screen-shot.png)
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,82 @@
 // Assignment code here
-var lowerCaseLetter = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCaseLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numericCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacter = ['!', '#', '$', '%', '&', '*', '+', '-', '='];
+const lowerCaseLetter = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numericCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialCharacter = ['!', '#', '$', '%', '&', '*', '+', '-', '='];
+const useLowerCase = window.confirm("Would you like to use lower case letters?");
+const useUpperCase = window.confirm ("Would you like to use upper case letters?");
+const useSpecialCharacter = window.confirm("Would you like to use special characters?");
+const useNumbers = window.confirm("Would you like to use numeric characters?");
+const passwordLength = window.prompt("Enter password length (between 8-128 characters");
 
-function getRandom(max) {
-    return (Math.floor(Math.random() * max))
-};
-
-const promptPasswordLength = function(){
-  const inputPasswordLength = prompt("Enter password length (between 8-128 characters")
-
-
-  if (userPasswordLength <8 || userPassLength > 128) {
-    alert("Please select a valid password length (between 8-128 characters)")
-    return promptPasswordLength();
-  }
-  else {
-    return userPasswordLength;
-  }
-};
-
-passwordLength = 
-var lowerCaseLetter = window.confirm("Would you like to use lower case letters?");
-var upperCaseLetter = window.confirm("Would you like to use upper case letters?");
-var numericCharacter = window.confirm('Would you like to use numbers?');
-var specialCharacter = window.confirm("Would you like to use special characters?");
-
-
-for (let i = 0; i < passwordLength; i++) {
-  if (lowerCaseLetterConfirm) {
-    randomLowerCase = getRandom(upperCaseLetter.length);
-};
-
-function generatePassword() {
-  
+let functionList = [];
+if(useLowerCase){
+  functionList.push(getLowerCaseLetter);
 }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+if(useUpperCase){
+  functionList.push(getUpperCaseLetter);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomNumber(max) {
+  return (Math.floor(Math.random() * max))
+};
+
+function getLowerCaseLetter(){
+  const tmp = getRandomNumber(lowerCaseLetter.length);
+  return lowerCaseLetter[tmp];
+}
+
+function getUpperCaseLetter(){
+  const tmp = getRandomNumber(upperCaseLetter.length);
+  return upperCaseLetter[tmp];
+}
+
+function getPasswordCharacter(){
+  const tmp = getRandomNumber(functionList.length);
+  return functionList[tmp]();
+}
+
+let finalPassword = '';
+for(let i = 0; i< passwordLength; i++){
+const char = getPasswordCharacter();
+finalPassword = finalPassword+char;
+}
+window.alert(finalPassword)
+
+// window.prompt = prompt("Enter password length (between 8-128 characters");
+//   userPasswordLength = parseInt(userPasswordLength)
+// console.log()
+
+// if (userPasswordLength <8 || userPassLength > 128) {
+//    alert("Please select a valid password length (between 8-128 characters)")
+//    return promptPasswordLength();
+// }
+//  else {
+//    return userPasswordLength;
+// };
+
+
+
+// for (let i = 0; i < passwordLength; i++) {
+//   if (lowerCaseLetterConfirm) {
+//     randomLowerCase = getRandom(lowerCaseLetter.length);
+// }
+// }
+
+// function generatePassword() {
+//   return "test";
+// }
+// // Get references to the #generate element
+// let generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   let password = generatePassword();
+//   let passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
