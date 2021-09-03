d9a5c693e8448c5328da50667e61a0b4f8f3c5c6
Patricia Nelson <urbanearthymama@gmail.com>
2021-09-02 21:32:40 -0500

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+let passwordEl = document.querySelector("#password");
+var button = document.getElementById('generate');
+
 const lowerCaseLetter = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperCaseLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numericCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numCharacter = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialCharacter = ['!', '#', '$', '%', '&', '*', '+', '-', '='];
 const useLowerCase = window.confirm("Would you like to use lower case letters?");
 const useUpperCase = window.confirm ("Would you like to use upper case letters?");
@@ -17,11 +21,11 @@ if(useUpperCase){
   functionList.push(getUpperCaseLetter);
 }
 if(useSpecialCharacter){
-  functionList.push(getSpecialCharacter);
+  functionList.push(useSpecialCharacter);
 }
 if(useNumbers){
-  functionList.push(getNumericCharacter);
-}
+functionList.push(getNumCharacter);
+};
 
 function getRandomNumber(max) {
   return (Math.floor(Math.random() * max))
@@ -30,17 +34,25 @@ function getRandomNumber(max) {
 function getLowerCaseLetter(){
   const tmp = getRandomNumber(lowerCaseLetter.length);
   return lowerCaseLetter[tmp];
-}
+};
 
 function getUpperCaseLetter(){
   const tmp = getRandomNumber(upperCaseLetter.length);
   return upperCaseLetter[tmp];
-}
+};
+
+function getSpecialCharacter(){
+  const tmp = getRandomNumber(specialCharacter.length);
+};
+
+function getNumCharacter(){
+  const tmp = getRandomNumber(numCharacter.length);
+};
 
 function getPasswordCharacter(){
   const tmp = getRandomNumber(functionList.length);
   return functionList[tmp]();
-}
+};
 
 let finalPassword = '';
 for(let i = 0; i< passwordLength; i++){
